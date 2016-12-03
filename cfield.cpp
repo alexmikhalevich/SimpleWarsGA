93a0d23b5999552ade356a9e1cca648885ba7aa0
maked0n <alex.mikhalevich@gmail.com>
2016-12-03 17:56:47 +0300

    Field added

--- cfield.cpp
+++ cfield.cpp
@@ -1,9 +1,11 @@
 #include "cfield.h"
 using namespace Representation;
 
-CShotRepresentation::CShotRepresentation(int xupperleft, int yupperleft, bool side) {
-	m_rect.x = xupperleft;
-	m_rect.y = yupperleft;
+CShotRepresentation::CShotRepresentation(int x, int y, bool side) {
+	m_x = x / CELL_SIZE;
+	m_y = y / CELL_SIZE;
+	m_rect.x = (x / CELL_SIZE) * CELL_SIZE;
+	m_rect.y = (y / CELL_SIZE) * CELL_SIZE;
 	m_rect.w = CELL_SIZE;
 	m_rect.h = CELL_SIZE;
 	m_side = side;
@@ -21,9 +23,19 @@ bool CShotRepresentation::side() const {
 	return m_side;
 }
 
-CUnitRepresentation::CUnitRepresentation(int xupperleft, int yupperleft, Unit::EUnitClass type, bool side) {
-	m_rect.x = xupperleft;
-	m_rect.y = yupperleft;
+int CShotRepresentation::x() const {
+	return m_x;
+}
+
+int CShotRepresentation::y() const {
+	return m_y;
+}
+
+CUnitRepresentation::CUnitRepresentation(int x, int y, Unit::EUnitClass type, bool side) {
+	m_x = x / CELL_SIZE;
+	m_y = y / CELL_SIZE;
+	m_rect.x = (x / CELL_SIZE) * CELL_SIZE; 
+	m_rect.y = (y / CELL_SIZE) * CELL_SIZE;
 	m_rect.w = CELL_SIZE;
 	m_rect.h = CELL_SIZE;
 	m_type = type;
@@ -42,14 +54,20 @@ bool CUnitRepresentation::side() const {
 	return m_side;
 }
 
-CField::CField(/*size_t vsize, size_t hsize*/) {
-	/*m_field.resize(hsize);
+int CUnitRepresentation::x() const {
+	return m_x;
+}
+
+int CUnitRepresentation::y() const {
+	return m_y;
+}
+
+CField::CField(size_t vsize, size_t hsize) {
+	m_field.resize(hsize);
 	for(size_t i = 0; i < hsize; ++i) {
-		m_field.resize(vsize);
-		for(size_t j = 0; j < vsize; ++j) 
-			m_field[i][j] = Unit::EUnitClass::NO_UNIT;
+		m_field[i].resize(vsize);
+		for(size_t j = 0; j < vsize; ++j) m_field[i][j] = NULL; 
 	}
-	*/
 	//m_next_element = m_elements.begin();
 }
 
@@ -67,12 +85,24 @@ void CField::renew() {
 }
 
 void CField::add_element(IRepresentation* representation) {
+	int x = representation->x();
+	int y = representation->y();
+	m_field[x][y] = representation;
 	m_elements.push_back(representation);
 }
 
 void CField::pop_element() {
-	if(!m_elements.empty()) m_elements.pop_back();
+	if(m_elements.empty()) return;
+	int x = (*(m_elements.end() - 1))->x();
+	int y = (*(m_elements.end() - 1))->y();
+	//delete m_field[x][y];
+	m_field[x][y] = NULL;
+	m_elements.pop_back();
 }
 
 void CField::renew_positions() {
+}
+
+bool CField::element_exists(int x, int y) const {
+	return (m_field[x / CELL_SIZE][y / CELL_SIZE] != NULL);
 }
--- cfield.h
+++ cfield.h
@@ -6,12 +6,14 @@
 #include "unit.hpp"
 
 namespace Representation {
-	constexpr int CELL_SIZE = 20;
+	constexpr int CELL_SIZE = 10;
 	class IRepresentation {
 		public:
 			virtual SDL_Rect* rect() = 0;
 			virtual Unit::EUnitClass type() const = 0;
 			virtual bool side() const = 0;
+			virtual int x() const = 0;
+			virtual int y() const = 0;
 	};
 	typedef std::vector<IRepresentation*>::iterator element;
 	class CUnitRepresentation : public IRepresentation {
@@ -19,37 +21,46 @@ namespace Representation {
 			SDL_Rect m_rect;
 			Unit::EUnitClass m_type;
 			bool m_side;
+			int m_x;
+			int m_y;
 		public:
-			CUnitRepresentation(int xupperleft, int yupperleft, Unit::EUnitClass type, bool side);
+			CUnitRepresentation(int x, int y, Unit::EUnitClass type, bool side);
 			SDL_Rect* rect();
 			Unit::EUnitClass type() const;
 			bool side() const;
+			int x() const;
+			int y() const;
 	};
 
 	class CShotRepresentation : public IRepresentation {
 		private:
 			SDL_Rect m_rect;
 			bool m_side;
+			int m_x;
+			int m_y;
 			//TODO: shot type?
 		public:
-			CShotRepresentation(int xupperleft, int yupperleft, bool side);
+			CShotRepresentation(int x, int y, bool side);
 			SDL_Rect* rect();
 			Unit::EUnitClass type() const;
 			bool side() const;
+			int x() const;
+			int y() const;
 	};
 
 	class CField {
 		private:
-			//std::vector<std::vector<Unit::EUnitClass>> m_field;
+			std::vector<std::vector<IRepresentation*>> m_field;
 			std::vector<IRepresentation*> m_elements;
 			element m_next_element;
 		public:
-			CField(/*size_t vsize, size_t hsize*/);
+			CField(size_t vsize, size_t hsize);
 			bool get_next(element* next);
 			void renew();
 			void add_element(IRepresentation* representation);
 			void pop_element();
 			void renew_positions();
+			bool element_exists(int x, int y) const;
 	};
 }
 #endif //CFIELD_H
