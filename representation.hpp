e6b3594ce7a688ba38a97da136e7a125c39dea1d
maked0n <alex.mikhalevich@gmail.com>
2016-12-04 01:05:36 +0300

    Major errors mixed

--- representation.hpp
+++ representation.hpp
@@ -1,7 +1,15 @@
 #ifndef REPRESENTATION_HPP
 #define REPRESENTATION_HPP
 
+#include <SDL2/SDL_rect.h>
+#include "unit.hpp"
+
 namespace Representation {
+	constexpr int CELL_SIZE = 10;
+	struct s_point {
+		int x;
+		int y;
+	};
 	class IRepresentation {
 		public:
 			virtual SDL_Rect* rect() = 0;
@@ -9,19 +17,19 @@ namespace Representation {
 			virtual bool side() const = 0;
 			virtual int x() const = 0;
 			virtual int y() const = 0;
+			virtual void add_x(int add) = 0;
+			virtual void add_y(int add) = 0;
 	};
-	typedef std::vector<IRepresentation*>::iterator element;
 	class CUnitRepresentation : public IRepresentation {
 		private:
 			SDL_Rect m_rect;
 			Unit::EUnitClass m_type;
 			bool m_side;
-			int m_x;
-			int m_y;
+			s_point m_position;
 		public:
 			CUnitRepresentation(int x, int y, Unit::EUnitClass type, bool side) {
-				m_x = x / CELL_SIZE;
-				m_y = y / CELL_SIZE;
+				m_position.x = x / CELL_SIZE;
+				m_position.y = y / CELL_SIZE;
 				m_rect.x = (x / CELL_SIZE) * CELL_SIZE; 
 				m_rect.y = (y / CELL_SIZE) * CELL_SIZE;
 				m_rect.w = CELL_SIZE;
@@ -32,20 +40,22 @@ namespace Representation {
 			SDL_Rect* rect() { return &m_rect; }
 			Unit::EUnitClass type() const { return m_type; }
 			bool side() const { return m_side; }
-			int x() const { return m_x; }
-			int y() const { return m_y; }
+			int x() const { return m_position.x; }
+			int y() const { return m_position.y; }
+			void add_x(int add) { m_position.x += add; }
+			void add_y(int add) { m_position.y += add; }
+
 	};
 	class CShotRepresentation : public IRepresentation {
 		private:
 			SDL_Rect m_rect;
 			bool m_side;
-			int m_x;
-			int m_y;
+			s_point m_position;
 			//TODO: shot type?
 		public:
 			CShotRepresentation(int x, int y, bool side) {
-				m_x = x / CELL_SIZE;
-				m_y = y / CELL_SIZE;
+				m_position.x = x / CELL_SIZE;
+				m_position.y = y / CELL_SIZE;
 				m_rect.x = (x / CELL_SIZE) * CELL_SIZE;
 				m_rect.y = (y / CELL_SIZE) * CELL_SIZE;
 				m_rect.w = CELL_SIZE;
@@ -55,8 +65,10 @@ namespace Representation {
 			SDL_Rect* rect() { return &m_rect; }
 			Unit::EUnitClass type() const { return Unit::EUnitClass::NO_UNIT; }
 			bool side() const { return m_side; }
-			int x() const { return m_x; }
-			int y() const {return m_y; }
+			int x() const { return m_position.x; }
+			int y() const {return m_position.y; }
+			void add_x(int add) { m_position.x += add; }
+			void add_y(int add) { m_position.y += add; }
 	};
 };
 #endif //REPRESENTATION_HPP
